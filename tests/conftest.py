f8e3d71365212b2146a5b93aedf9de2f56ef12ac
hacking <western.ant2@gmail.com>
2021-12-22 00:53:15 +0200

    - created app factory with - wrote tests

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+from shutil import copy
+
+import pytest
+
+
+from app import create_app
+
+
+@pytest.fixture
+def client(tmpdir):
+    copy("test.db", tmpdir.dirpath())
+    temp_db_file = f"sqlite:///{tmpdir.dirpath()}/test.db"
+    app = create_app(temp_db_file)
+    # app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = temp_db_file
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
